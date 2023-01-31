9193c2d8bea382930b3552418d6d64b0c764e3f9
Shtefan <86320158+shtefannuzhdenko@users.noreply.github.com>
2023-01-31 10:24:19 -0600

    Update FirstGitHubClass.java
    hey changed to hellooo

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub! :)");
+        System.out.println("Helloooo GitHub! :)");
         System.out.println(1+1);
     }
 }
